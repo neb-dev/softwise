c0a2abbda7fc14883e50d6d1f831e914f2f6007f
neb <56274697+neb-dev@users.noreply.github.com>
2021-02-19 20:00:11 -0700

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,3 +1,21 @@
+var ssnInput = document.getElementById("ssn");
+var dobInput = document.getElementById("dob");
+var submitButton = document.getElementById("submit");
+var body = document.getElementsByTagName("BODY")[0];
+
+submitButton.addEventListener("load", function() {
+  submitButton.disabled = true;
+});
+
 function validateForm() {
   "use strict";
+  if (ssnInput.value.length === 4 && dob.value.length === 10) {
+    submitButton.className = "enable";
+  } else {
+    submitButton.disabled = true;
+    submitButton.className = "disable";
+  }
 }
+
+ssnInput.addEventListener("keyup", validateForm);
+dobInput.addEventListener("keyup", validateForm);
